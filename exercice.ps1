10ee87a3f82c26f54dd94e8d0b48b7869849985a
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2026-01-02 21:40:13 +0100

    Create exercice.ps1

---
+++ exercice.ps1
@@ -0,0 +1,12 @@
+Function Get-ResistorLabel() {
+    <#
+    EXAMPLE
+    Get-ResistorLabel -Colors @("red", "white", "blue")
+    Return: "29 megaohms"
+     #>
+    [CmdletBinding()]
+    Param(
+        [string[]]$Colors
+    )
+    Throw "Please implement this function"
+}
